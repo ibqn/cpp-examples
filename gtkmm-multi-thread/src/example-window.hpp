d12f95ea61460c33ccc3d7f32c30b470480f8891
Evgeny Bobkin <evgeny.bobkin@gmail.com>
2018-05-25 16:18:44 +0200

    wip. multi thread example

---
+++ gtkmm-multi-thread/src/example-window.hpp
@@ -0,0 +1,46 @@
+#ifndef GTKMM_EXAMPLE_WINDOW_HPP
+#define GTKMM_EXAMPLE_WINDOW_HPP
+
+
+#include <thread>
+#include <gtkmm.h>
+
+#include "example-worker.hpp"
+
+
+class ExampleWindow : public Gtk::Window
+{
+public:
+    ExampleWindow();
+
+    // Called from the worker thread.
+    void notify();
+
+private:
+    // Signal handlers.
+    void on_start_button_clicked();
+    void on_stop_button_clicked();
+    void on_quit_button_clicked();
+
+    void update_start_stop_buttons();
+    void update_widgets();
+
+    // Dispatcher handler.
+    void on_notification_from_worker_thread();
+
+    // Member data.
+    Gtk::Box m_VBox;
+    Gtk::ButtonBox m_ButtonBox;
+    Gtk::Button m_ButtonStart;
+    Gtk::Button m_ButtonStop;
+    Gtk::Button m_ButtonQuit;
+    Gtk::ProgressBar m_ProgressBar;
+    Gtk::ScrolledWindow m_ScrolledWindow;
+    Gtk::TextView m_TextView;
+
+    Glib::Dispatcher m_Dispatcher;
+    ExampleWorker m_Worker;
+    std::thread* m_WorkerThread;
+};
+
+#endif // GTKMM_EXAMPLE_WINDOW_HPP
---
+++ gtkmm-multi-thread/src/example-worker.hpp
@@ -0,0 +1,32 @@
+#ifndef GTKMM_EXAMPLE_WORKER_HPP
+#define GTKMM_EXAMPLE_WORKER_HPP
+
+#include <gtkmm.h>
+#include <thread>
+#include <mutex>
+
+
+class ExampleWorker
+{
+public:
+    ExampleWorker();
+
+    // Thread function.
+    void do_work(ExampleWindow* caller);
+
+    void get_data(double* fraction_done, Glib::ustring* message) const;
+    void stop_work();
+    bool has_stopped() const;
+
+private:
+    // Synchronizes access to member data.
+    mutable std::mutex m_Mutex;
+
+    // Data used by both GUI thread and worker thread.
+    bool m_shall_stop;
+    bool m_has_stopped;
+    double m_fraction_done;
+    Glib::ustring m_message;
+};
+
+#endif // GTKMM_EXAMPLE_WORKER_HPP
